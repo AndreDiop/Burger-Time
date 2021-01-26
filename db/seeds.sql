83f0cf95e859b3bd775fefd8613b2e9af3105cc2
Andre Diop <itsjustandre@gmail.com>
2021-01-25 21:36:31 -0500

    Schema and seeds file created

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Cowboy Burger", false),
+("Fox Bros. Burger", true),
+("Impossible Burger", false),
+("Big Mac Burger", false),
+("Baconator Burger", false),
+("Hardee's Thick Burger", false)
