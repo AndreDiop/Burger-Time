a78a103472a2e6c1b32c80cc1b06002b76ebb911
Andre Diop <itsjustandre@gmail.com>
2021-01-25 21:48:45 -0500

    Create connection.js
    - mysql connection established

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "trapstar",
+  database: "burgers_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("error connecting to you cause of" + err);
+  }
+  console.log("Get your motor running on " + connection.threadId);
+});
+
